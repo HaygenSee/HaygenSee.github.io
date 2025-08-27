2a995245b5c44396fbc1c6e398c4bd8b92b39dd9
HaygenSee <91530348+HaygenSee@users.noreply.github.com>
2025-08-27 02:00:32 +0000

    Deploying to gh-pages from @ HaygenSee/HaygenSee.github.io@0ab69d60e4c9ccf1a816dc2dbfcb19f82f67face 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "Here&#39;s the general summary of my CV. Download for the PDF file is available on the PDF icon on the right.",
